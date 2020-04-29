07554ba6053b75a2946956bf2b3e7af695048adc
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-29 06:50:26 +0000

    making user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 4 }, uniqueness: { case_sensitive: true }
+  has_many :articles 
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :user do
-    name { 'Juha' }
+    name { "Juha" } 
   end
 end
