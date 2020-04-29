5dd89a828604c452befd5cafa34b8c5646e74bbb
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-29 21:13:08 +0000

    removing migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_29_144509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -81,9 +60,4 @@ ActiveRecord::Schema.define(version: 2020_04_29_144509) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "articles", "users"
-  add_foreign_key "artilces", "users"
-  add_foreign_key "votes", "articles"
-  add_foreign_key "votes", "users"
 end
