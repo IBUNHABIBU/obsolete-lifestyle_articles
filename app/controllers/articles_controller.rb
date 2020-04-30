638933961d5e55b875807fb6b958f8d80cbe59c5
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-30 05:36:12 +0000

    adding image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :content, :user_id)
+      params.require(:article).permit(:title, :content, :image, :user_id)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
