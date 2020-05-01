04a342c9d94a080d1791f7522d961b932c2b02bf
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-01 14:20:17 +0000

    modifying controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :content, :image, :user_id)
+      params.require(:article).permit(:title, :content, :image, :user_id, category_ids: [])
     end
 end
