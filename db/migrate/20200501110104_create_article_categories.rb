7affc25b22bf1baa7df37a63c4dcb4355e8008c4
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-01 11:08:24 +0000

    modifying gem and migration

--- db/migrate/20200501110104_create_article_categories.rb
+++ db/migrate/20200501110104_create_article_categories.rb
@@ -1,8 +1,8 @@
 class CreateArticleCategories < ActiveRecord::Migration[5.2]
-  # def change
-  #   create_table :article_categories do |t|
+  def change
+    create_table :article_categories do |t|
 
-  #     t.timestamps
-  #   end
-  # end
+      t.timestamps
+    end
+  end
 end
