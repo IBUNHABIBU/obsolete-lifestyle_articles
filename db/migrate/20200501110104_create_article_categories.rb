1c3f76ec6c28d0a3a4d951adee9574452f49b95f
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-01 11:56:57 +0000

    adding association model

--- db/migrate/20200501110104_create_article_categories.rb
+++ db/migrate/20200501110104_create_article_categories.rb
@@ -1,7 +1,9 @@
 class CreateArticleCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :article_categories do |t|
+      t.integer :article_id
 
+      t.integer :category_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_105834) do
+ActiveRecord::Schema.define(version: 2020_05_01_110104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "artilces", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.bigint "user_id"
