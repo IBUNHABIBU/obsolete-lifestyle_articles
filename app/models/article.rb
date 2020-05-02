99b8ed815025827bdab7656433431e64c0e0fff8
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-02 17:35:19 +0000

    adding like icon

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,5 @@ class Article < ApplicationRecord
   has_one_attached :image
   has_many :article_categories
   has_many :categories, through: :article_categories
-  acts_as_voter
+  acts_as_votable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4 }, uniqueness: { case_sensitive: true }
   has_many :articles
   has_many :votes
-  acts_as_votable
+  acts_as_voter
 end
