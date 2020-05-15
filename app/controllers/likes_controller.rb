89d111e3c792b8800445afd0f62cef96de0a8ce6
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-15 20:35:37 +0000

    restyling the most recent article

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,13 @@ class LikesController < ApplicationController
   def create
       @article = Article.find(params[:article_id])
       @article.likes.create!(user: current_user)
-      redirect_to @article , notice:"Glad! you liked it!"
+       flash[:success]= "Glad! you liked it!"
+      redirect_to @article 
   end
   def destroy
     like = current_user.likes.find(params[:id])
     like.destroy
+      flash[:danger]= "Sorry! you unliked it!"
     @article = Article.find(params[:article_id])
     redirect_to @article 
   end
