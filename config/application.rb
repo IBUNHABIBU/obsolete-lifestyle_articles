37ed77efab557ebb027a57d565aca0be808678bd
IBUNHABIBU <ibunhabib@gmail.com>
2020-05-21 12:23:17 +0000

    fixing heroku

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module LifestyleArticles
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
