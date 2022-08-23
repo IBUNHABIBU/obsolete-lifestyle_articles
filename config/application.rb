182a90fc3dcd48d80a53c8135f6632fa1023218b
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-23 12:14:41 +0300

    Upgrade to rails 6

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,6 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,18 +8,15 @@ Bundler.require(*Rails.groups)
 
 module LifestyleArticles
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
-    config.app_generators.scaffold_controller :responders_controller
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.serve_static_assets = true
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  
-  root 'articles#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'signup', to: 'users#new'
-  resources :users
-  resources :categories
-  resources :articles do
-    # member do
-    #   put 'like' => 'articles#vote'
-    # end
-    resources :likes
-  end
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
