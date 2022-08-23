767a5eb9c92d56ab63d5703f7c63da78086b2a8a
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-23 11:13:51 +0300

    Finish the pending test

--- spec/models/article_category_spec.rb
+++ spec/models/article_category_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ArticleCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Article Associations' do
+    it { should belong_to(:category) }
+    it { should belong_to(:article) }
+  end
 end
