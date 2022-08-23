340d2e15edcfb0b6fce349f34542802ff1188533
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-23 12:23:07 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  resources :users
+  resources :categories
+  resources :articles do
+    # member do
+    #   put 'like' => 'articles#vote'
+    # end
+    resources :likes
+  end
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
