a5238982c6b85f45a1a592f159f3428768922f99
IBUNHABIBU <ibunhabib@gmail.com>
2023-01-13 14:27:46 +0300

    Add eager loading to application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module LifestyleArticles
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load = true
   end
 end
