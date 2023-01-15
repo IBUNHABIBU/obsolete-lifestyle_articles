76e3137010b67588355a4226c8bd163df375f3f5
IBUNHABIBU <ibunhabib@gmail.com>
2023-01-16 00:51:35 +0300

    Add sass reload

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module LifestyleArticles
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.assets.enabled = true
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # config.eager_load = true
